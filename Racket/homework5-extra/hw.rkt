21fb13b64c9a5eee5d136e6b7dd09a3857328d06
arufth <carlos256800@gmail.com>
2024-11-19 18:14:21 -0600

    add template homework5-extra

---
+++ Racket/homework5-extra/hw.rkt
@@ -0,0 +1,104 @@
+#lang racket
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+; Racket structs
+
+; A binary tree is either (btree-leaf) or a Racket value built from btree-node where the left and right fields are
+; both binary trees.
+
+(struct btree-leaf () #:transparent)
+(struct btree-node (value left right) #:transparent)
+
+; 1) Write a function tree-height that accepts a binary tree and evaluates to a height of this tree. The height of a
+; tree is the lenght of the longest path to a leaf. Thus the height of a leaf is 0
+
+
+
+; 2) Write a function sum-tree that takes binary tree and sums all the values in all the nodes. (Assume the values
+; fields all hold numbers, i.e., values that can pass to +)
+
+
+
+; 3) Write prune-at-v that takes a binary tree t and a value v and produces a new binary tree with structure the same
+; as t except any node with value equal to v (use Racket's equal?) is replaced (along with all its descendants) by a
+; leaf
+
+
+
+; 4) Write a function well-formed-tree? that takes any value and returns #t if and only if the value is legal binary
+; tree as defined above
+
+
+
+; 5) Write a function fold-tree that takes a two-argument function, an initial accumulator, and a binary tree and
+; implements a fold over the tree, applying the function to all the values.
+;
+; For example:
+; (fold-tree (lambda (x y) (+ x y 1)) 7
+;   (btree-node 4 (btree-node 5 (btree-leaf) (btree-leaf)) (btree-leaf)))
+; would evaluates to 18. You can traverse the tree in any order you like (though it does affect the result af a call
+; to fold-tree if the function passed isn't associative)
+
+
+
+; 6) Reimplement fold-tree as a curried function
+
+
+
+; Dynamic typing
+
+; 7) Write a function crazy-sum that takes a list of numbers and adds all together, starting from the left. There's a
+; twist, however. The list is allowed to contain functions in addition to numbers. Whenever an element of a list is a
+; function, you should start using it to combine all the following numbers in a list instead of +. You may assume that
+; the list is non-empty and contains only numbers and binary functions suitable for operating on two numbers. Further
+; assume the first list element is a number.
+;
+; For example:
+; (crazy-sum (list 10 * 6 / 5 -3)) evaluates to 9. Note: it may superficially look like the function implements infix
+; syntax for arithmetic expressions, but that's not really the case
+
+
+
+
+; 8) Write a function either-fold that is like fold for lists of brinay trees as defined above except that it works
+; for both of them. Given an appropiate errer message if the third argument to either-fold is neither a list or a
+; binary-tree
+
+
+
+; 9) Write a function flatten that takes a list and flattens its internal structure, merging all the lists inside into
+; a single flat list. This should work for lists nested to arbitrary depth.
+;
+; For example:
+; (flatten (list 1 2 (list (list 3 4) 5 (list (list 6) 7 8)) 9 (list 10))) should evaluate to
+; (list 1 2 3 4 5 6 7 8 9 10)
+
+
+; Using lambda-calculus ideas to remove features form MUPL programs
+
+; Like Racket itself, MUPL (the programming language from this section's homework assignment) is essentially a
+; superset of untyped lambda calculus. "Lambda calculus" may sound scary, but it's essentially a very simple
+; programming language -- it really doesn't have anything in it, apart from anonymous functions and functions calls!
+; Of course, that makes it very inconvenient to program in, which is also why real programming languages usually
+; supply all sorts of bells and whistels, liki additional language constructs and data types like booleans and numbers
+;
+; Nonetheless, untyped lambda calculus is Turing-complete, so we can actually represent things like numbers and
+; booleans using nothing but functions. In these problems we'll do some of that in MUPL.
+
+; 10) Notice that MUPL doesn't need mlet: Anywhere we have (mlet name e body), we can use (call (fun #f name body) e)
+; instead and get the same result. Write a Racket function remove-lets that takes a MUPL program and produces an
+; equivalent MUPL program that does not contain mlet
+
+
+
+; 11) [more challenging] Now we will do something even more clever: remove pairs by using closures environments as
+; another way to "hold" two pieces of data. Instead of using (apair e1 e2), we can use
+; (mlet "_x" e1 (mlet "_y" e2 (fun #f "_f" (call (call (var "_f") (var "_x")) (var "_y")))))
+
+
+
+
+
+
+
+
